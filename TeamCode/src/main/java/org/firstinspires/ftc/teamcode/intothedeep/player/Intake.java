b1cbf5fec9d938cdc7b70b03595ada0c23ccf27c
Annika keefer <AnnikaKeefer@icloud.com>
2024-10-08 18:27:52 -0400

    worked on mecanum drive.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/player/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/player/Intake.java
@@ -1,24 +1,28 @@
 package org.firstinspires.ftc.teamcode.intothedeep.player;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Intake {
     static LinearOpMode _linearOpMode;
-    static CRServo _servo;
+    static Servo _mClaw;
+    public final static double _clawMinimumRange = 0.15;
+    public final static double _clawMaxRange = 1.5;
 
     public static void initialize(LinearOpMode linearOpMode) {
         _linearOpMode = linearOpMode;
-        _servo = linearOpMode.hardwareMap.get(CRServo.class, "crs");
+        _mClaw = linearOpMode.hardwareMap.servo.get("claw");
+        _mClaw.setPosition(_clawMinimumRange);
+        _linearOpMode.telemetry.addData("ClawPosition", _mClaw.getPosition());
     }
 
     public static void run() {
         if (_linearOpMode.gamepad2.left_bumper) {
-            _servo.setPower(1);
+            _mClaw.setPosition(_clawMinimumRange);
         } else if (_linearOpMode.gamepad2.right_bumper) {
-            _servo.setPower(-1);
-        } else {
-            _servo.setPower(0);
+            _mClaw.setPosition(_clawMaxRange);
         }
+
+        _linearOpMode.telemetry.addData("ClawPosition", _mClaw.getPosition());
     }
 }
