969a5d48b399cc00cee608c7d7236ceb5d059ea8
Annika keefer <AnnikaKeefer@icloud.com>
2024-11-11 19:23:42 -0500

    adjusted Red_Left_Basket numbers, added a sleep, etc.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Left_Basket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_Left_Basket.java
@@ -19,6 +19,8 @@ public class Red_Left_Basket extends LinearOpMode {
         Intake.open();
         Latcher.initialize(this);
         Latcher.close();
+        telemetry.addLine("ready");
+        telemetry.update();
         waitForStart();
 
         ElapsedTime runtime = new ElapsedTime();
@@ -31,19 +33,19 @@ public class Red_Left_Basket extends LinearOpMode {
         SubSystem.DriveAll(0, 0, 0, 0,0 ,0,1,1000);
         SubSystem.DriveAll(0.4, 400, 0, 0,0 ,0,1,1000);
         Log.i("5960", runtime.milliseconds() +  ": Strafe towards the elements on the floor.");
-        SubSystem.StrafeAll(-0.4, 2550, 0, 0,0 ,0,1,0);
+        SubSystem.StrafeAll(-0.4, 2525, 0, 0,0 ,0,1,0);
 
-        SubSystem.DriveAll(0.4, -335, 0, 0,0 , 0,0,0);
+        SubSystem.DriveAll(0.4, -340, 0, 0,0 , 0,0,0);
         Intake.close();
         sleep(300);
 
-        SubSystem.TurnAll(0.4, 143, 1, -3350, 0, 0, 0, 0);
-        SubSystem.DriveAll(0.4, -1150, 1, -3350,1 ,-850,0,0);
+        SubSystem.TurnAll(0.4, 145, 1, -3350, 0, 0, 0, 0);
+        SubSystem.DriveAll(0.4, -1140, 1, -3350,1 ,-850,0,0);
         Intake.open();
         sleep(300);
 
-        SubSystem.TurnAll(0.4, -143, 0, 0, 1, -10, 0, 0);
-        SubSystem.DriveAll(0.4, -1130, 1, 0,0 ,0,0,0);
+        SubSystem.TurnAll(0.4, -137, 0, 0, 1, -10, 0, 0);
+        SubSystem.DriveAll(0.4, -770, 1, 0,0 ,0,0,0);
 
 //        SubSystem.TurnAll(0.4, -141, 0, 0, 0, 0, 0, 0);
 //        SubSystem.DriveAll(0.4, -185, 0.7, -819,0 ,0,0,0);
@@ -52,9 +54,10 @@ public class Red_Left_Basket extends LinearOpMode {
         Intake.close();
         sleep(300);
 
-        SubSystem.TurnAll(0.4, 165, 1, -3350, 1, 0, 0, 0);
+        SubSystem.DriveAll(0, -0, 1, -3350,0 ,0,0,0);
+        SubSystem.TurnAll(0.4, 155, 1, -3350, 1, 0, 0, 0);
 //        SubSystem.DriveAll(0, 0, 0, 0,1 ,-850,0,0);
-        SubSystem.DriveAll(0.4, -1130, 0, 0,1 ,-850,0,0);
+        SubSystem.DriveAll(0.4, -1100, 0, 0,1 ,-850,0,0);
         Intake.open();
         sleep(300);
 
@@ -62,6 +65,7 @@ public class Red_Left_Basket extends LinearOpMode {
         SubSystem.DriveAll(0.7, -2000, 1, 0,1 ,1,0,0);
         SubSystem.StrafeAll(-0.7, 250, 0, 0,0 ,0,0,0);
         SubSystem.DriveAll(0.7, 2000, 0, 0,0 ,0,0,0);
+        sleep(30000);
 //        SubSystem.DriveAll(0.4, 200, 0, 0,0 ,0,0,0);
     }
 }
